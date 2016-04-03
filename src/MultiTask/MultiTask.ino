dd0c2bbe9185f282b87f06e6d03fa8ed2709c7b5
sundy <sundycoder@gmail.com>
2016-04-03 16:41:33 +0800

    1.Add call back function for multi tasking.

--- src/MultiTask/MultiTask.ino
+++ src/MultiTask/MultiTask.ino
@@ -2,22 +2,22 @@
 #include "pt.h"
 static struct pt pt1, pt2;
 
-static int protothread1(struct pt *pt) {
+static int protothread1(struct pt *pt,int time) {
   static unsigned long timestamp = 0;
   PT_BEGIN(pt);
   while (1) {
-    PT_WAIT_UNTIL(pt, millis() - timestamp > 500);
+    PT_WAIT_UNTIL(pt, millis() - timestamp > time);
     Serial.println("Thread1......");
     timestamp = millis(); // take a new timestamp
   }
   PT_END(pt);
 }
 
-static int protothread2(struct pt *pt) {
+static int protothread2(struct pt *pt,int time) {
   static unsigned long timestamp = 0;
   PT_BEGIN(pt);
   while (1) {
-    PT_WAIT_UNTIL(pt, millis() - timestamp > 1000);
+    PT_WAIT_UNTIL(pt, millis() - timestamp > time);
     Serial.println("Thread2......");
     timestamp = millis(); // take a new timestamp
   }
@@ -31,10 +31,14 @@ void setup() {
   PT_INIT(&pt2);
 }
 
+void xTaskCreate(int (*xTaskName)(struct pt *,int),struct pt *ptr,int time) {  
+  (* xTaskName)(ptr,time);
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-    protothread1(&pt1);
-    protothread2(&pt2);
+  xTaskCreate(protothread1,&pt1,1000);
+  xTaskCreate(protothread2,&pt2,1000);
 }
 
 
